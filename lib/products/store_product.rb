8011f65a8c87d66ee148b64f25f95e5db9e51ba0
Sebastian Siemianowski <rudph2@gmail.com>
2016-10-17 05:24:22 +0100

    adds product store

--- lib/products/store_product.rb
+++ lib/products/store_product.rb
@@ -1,5 +1,31 @@
+require "json"
+
 module Products
   class StoreProduct
+    attr_reader :id, :name, :price, :product_code
 
+    def self.data
+      JSON.parse(File.read(File.expand_path('../products/store_products.json', File.dirname(__FILE__))))["store_products"]
+    end
+    private_class_method :data
+
+    def self.all
+      data.map { |product| new(product) }
+    end
+
+    def self.find(id)
+      all.detect { |product| product.id == id }
+    end
+
+
+
+
+
+    def initialize(raw_product)
+      @name = raw_product["name"]
+      @id = raw_product["id"].to_i
+      @price = raw_product["price"].to_f
+      @product_code = raw_product["product_code"]
+    end
   end
 end
--- spec/lib/products/store_product_spec.rb
+++ spec/lib/products/store_product_spec.rb
@@ -1,5 +1,71 @@
+require_relative '../../../lib/products/store_product'
+
 module Products
   describe StoreProduct do
+    subject(:product) do
+      StoreProduct.new(
+        "id" => '1',
+        "name" => 'foo bar',
+        "price" => '11.30',
+        "product_code" => '001'
+      )
+    end
+
+    it "has an id" do
+      expect(product.id).to eq 1
+    end
+
+    it "has text" do
+      expect(product.name).to eq "foo bar"
+    end
+
+    it "has price" do
+      expect(product.price).to eq 11.30
+    end
+
+    it "has product code" do
+      expect(product.product_code).to eq '001'
+    end
+
+    describe ".all" do
+      subject(:all) { Products::StoreProduct.all }
+
+      it "loads 3 products from the datastore" do
+        expect(all.size).to eq 3
+      end
+
+      it "instantates instances of store_product" do
+        all.all? { |product| expect(product).to be_a StoreProduct }
+      end
+
+      it "creates the store products with the correct data" do
+        product_names = all.map(&:name)
+        expect(product_names).to eq(["Lavender heart", "Personalised cufflinks", "Kids T-shirt "])
+      end
+    end
+
+    describe ".find" do
+      subject(:found) { StoreProduct.find(1) }
+      let(:products) { [product].compact }
+
+      before(:each) { allow(StoreProduct).to receive(:all).and_return(products) }
+
+      context "when the product can't be found" do
+        let(:product) {}
+
+        it "returns nil" do
+          expect(found).to be_nil
+        end
+      end
+
+      context "when there is a product to find" do
+        let(:product) { StoreProduct.new("id" => "1", "text" => "foo") }
+
+        it "finds the product" do
+          expect(found).to eq(product)
+        end
+      end
+    end
 
   end
 end
